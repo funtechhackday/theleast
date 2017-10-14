8d5d9016d43904d53cf4439fd3cce5dbbad5e211
Иван <if1242@yandex.ru>
2017-10-14 13:10:35 +0300

    Основной скрипт для игры

---
+++ js/game.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+function randomInteger(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+}
+
+var clicked = 0;  
+
+$(".num").click(function(){
+	clicked = 1;   
+	var tds = document.getElementsByTagName("td");
+	var min = Number(tds[0].innerHTML);
+	for (var i = 0; i < tds.length; i++) {
+		if (min > Number(tds[i].innerHTML)) {
+			min = Number(tds[i].innerHTML);
+		}
+	}
+	var clicknum = $(this).html();
+	var scorestr = document.getElementById('score').innerHTML;
+	var score = Number(scorestr);
+	if (clicknum == min) {
+		score = score + 10;
+		document.getElementById('score').innerHTML = + score;
+	} else {
+		 clearInterval();
+		 $('.game-start').hide();
+		 $('.game-table').hide();
+		 $('.game-over').show();
+		 $('.play-again').show()
+	}
+	var array = [];
+	array[0] = randomInteger(0, 25);
+	for (i = 1; i < tds.length; i++) {
+		array[i] = randomInteger(0, 25);
+		for (var j = 0; j < i; j++) {
+			if (array[i] == array[j]) {
+				array[j] = array[j] + 1 + j;
+			}
+		}
+		
+	}
+	
+	for (i = 0; i < tds.length; i++) {
+		tds[i].innerHTML = Number(array[i]);
+	}
+}); 
+
+setInterval(function() {                                 
+	if (clicked == 1) {
+      clicked = 0;
+    } else {
+      $('.game-start').hide();
+	  $('.game-table').hide();
+	  $('.game-over').show();
+	  $('.play-again').show();
+	  clearInterval();
+    }
+  }, 2000);
+});
